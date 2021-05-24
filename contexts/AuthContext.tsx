d446d0a42a2ed291abf3d133c7ce972945646cd1
Igor Barbosa <igorfbarbosa@gmail.com>
2021-05-24 16:56:59 -0300

    jwt_auth_added

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -1,4 +1,5 @@
-import { createContext, ReactNode, useState } from 'react';
+import { createContext, ReactNode, useEffect, useState } from 'react';
+import { setCookie, parseCookies, destroyCookie } from 'nookies';
 import Router from 'next/router';
 import { api } from '../services/api';
 
@@ -25,9 +26,30 @@ type AuthProviderProps = {
 
 export const AuthContext = createContext({} as AuthContextData);
 
+export function signOut() {
+    destroyCookie(undefined, 'nextauth.token')
+    destroyCookie(undefined, 'nextauth.refreshToken')
+
+    Router.push('/');
+}
+
 export function AuthProvider({ children }: AuthProviderProps) {
     const [user, setUser] = useState<User>();
     const isAuthenticated = !!user;
+
+    useEffect(() => {
+        const { 'nextauth.token': token } = parseCookies()
+
+        if (token) {
+            api.get('/me').then(response => {
+                const { email, permissions, roles } = response.data;
+
+                setUser({ email, permissions, roles });
+            }).catch(() => {
+                signOut();
+            });
+        }
+    }, []);
 
     async function signIn({ email, password }: SignInCredentials) {
         try {
@@ -36,13 +58,25 @@ export function AuthProvider({ children }: AuthProviderProps) {
                 password
             });
 
-            const { permissions, roles } = response.data;
+            const { token, refreshToken, permissions, roles } = response.data;
+
+            setCookie(undefined, 'nextauth.token', token, {
+                maxAge: 60 * 60 * 24 * 30,
+                path: '/'
+            });
+
+            setCookie(undefined, 'nextauth.refreshToken', refreshToken, {
+                maxAge: 60 * 60 * 24 * 30,
+                path: '/'
+            });
 
             setUser({
                 email,
                 permissions,
                 roles
             });
+
+            api.defaults.headers['Authorization'] = `Bearer ${token}`;
 
             Router.push('/dashboard');
         } catch(err) {
